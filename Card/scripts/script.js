071294e034d8a40cefa09885266f4562c1de2f42
mm-nay <nay@meshminds.com>
2020-07-14 14:55:18 +0800

    Disable Native UI picker

--- Card/scripts/script.js
+++ Card/scripts/script.js
@@ -1,41 +1,41 @@
-const NativeUI = require('NativeUI');
-const Textures = require('Textures');
-const Patches = require('Patches');
+// const NativeUI = require('NativeUI');
+// const Textures = require('Textures');
+// const Patches = require('Patches');
 
-Promise.all([
-    Textures.findFirst('icon_1'),
-    Textures.findFirst('icon_2'),
-    Textures.findFirst('icon_3'),
-]).then(onReady);
+// Promise.all([
+//     Textures.findFirst('icon_1'),
+//     Textures.findFirst('icon_2'),
+//     Textures.findFirst('icon_3'),
+// ]).then(onReady);
 
 
-function onReady(assets) {
+// function onReady(assets) {
 
-    const texture0 = assets[0];
-    const texture1 = assets[1];
-    const texture2 = assets[2];
+//     const texture0 = assets[0];
+//     const texture1 = assets[1];
+//     const texture2 = assets[2];
 
-    const picker = NativeUI.picker;
+//     const picker = NativeUI.picker;
 
-    const index = 0;
-    const selection = 0;
+//     const index = 0;
+//     const selection = 0;
 
-    const configuration = {
+//     const configuration = {
 
-      selectedIndex: index,
+//       selectedIndex: index,
 
-      items: [
-        {image_texture: texture0},
-        {image_texture: texture1},
-        {image_texture: texture2}
-      ]
+//       items: [
+//         {image_texture: texture0},
+//         {image_texture: texture1},
+//         {image_texture: texture2}
+//       ]
 
-    };
+//     };
 
-    picker.configure(configuration);
-    picker.visible = true;
+//     picker.configure(configuration);
+//     picker.visible = true;
 
-    picker.selectedIndex.monitor().subscribe(function(index) {
-      Patches.inputs.setScalar('selection', index.newValue);
-    });
-}
+//     picker.selectedIndex.monitor().subscribe(function(index) {
+//       Patches.inputs.setScalar('selection', index.newValue);
+//     });
+// }
